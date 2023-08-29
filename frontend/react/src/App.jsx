05b7daef8cf63b48b215bd181ab0df364f7c9b1b
kushalshrestha <kushalshr@gmail.com>
2023-08-29 18:51:40 -0500

    feat: Frontend setup

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,117 @@
+import UserProfile from "./UserProfile.jsx";
+import {useState, useEffect} from 'react';
+
+const users = [
+    {
+        name: "Jamila",
+        age: 22,
+        gender: "FEMALE"
+    },
+    {
+        name: "Ana",
+        age: 45,
+        gender: "FEMALE"
+    },
+    {
+        name: "Alex",
+        age: 18,
+        gender: "MALE"
+    },
+    {
+        name: "Bilal",
+        age: 27,
+        gender: "MALE"
+    },
+    {
+        name: "Bob",
+        age: 27,
+        gender: "MALE"
+    }
+]
+
+const UserProfiles = ({users}) => (
+    <div>
+        {users.map((user, index) => (
+            <UserProfile
+                key={index}
+                name={user.name}
+                age={user.age}
+                gender={user.gender}
+                imageNumber={index}
+            />
+        ))}
+    </div>
+)
+
+function App() {
+
+    const [counter, setCounter] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 4000)
+        return () => {
+            console.log("cleanup functions")
+        }
+    }, [])
+
+    if(isLoading) {
+        return "loading...";
+    }
+
+    return (
+        <div>
+            <button
+                onClick={() => setCounter(prevCounter => prevCounter + 1)}>
+                Increment counter
+            </button>
+            <h1>{counter}</h1>
+            <UserProfiles users={users}/>
+        </div>
+    )
+}
+
+export default App
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+//
+// function App() {
+//   const [count, setCount] = useState(0)
+//
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+//
+// export default App
